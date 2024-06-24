e8884cc39ff8655094cead862e6529b6f5f0ebdc
bishtdev <bishtdev030@gmail.com>
2024-06-24 17:31:40 +0530

    setting up the header

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">portfolio website</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
