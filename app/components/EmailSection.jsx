1516dfbe78d6b55e70fc43da7b8c9944256ecb48
bishtdev <bishtdev030@gmail.com>
2024-06-27 01:16:39 +0530

    updated

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -7,39 +7,9 @@ import Image from "next/image";
 
 const EmailSection = () => {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-          email: e.target.email.value,
-          subject: e.target.subject.value,
-          message: e.target.message.value,
-        };
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
-    
-        // Form the request for sending data to the server.
-        const options = {
-          // The method is POST because we are sending data.
-          method: "POST",
-          // Tell the server we're sending JSON.
-          headers: {
-            "Content-Type": "application/json",
-          },
-          // Body of the request is the JSON data we created above.
-          body: JSONdata,
-        };
-    
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
-    
-        if (response.status === 200) {
-          console.log("Message sent.");
-          setEmailSubmitted(true);
-        }
-      };
 
   return (
-    <section className="grid md:grid-cols-3 my-12 md:my-12 py-24 gap-4">
+    <section id="contact" className="grid md:grid-cols-3 my-12 md:my-12 py-24 gap-4">
       <div>
         <h5 className="text-2xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -49,12 +49,14 @@ const HeroSection = () => {
                 Online CV
               </span>
             </button> */}
-            <button className="cursor-pointer text-white font-bold relative text-[17px] mr-8 px-6 py-2 text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px] hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-violet-700 focus:ring-violet-700 z-10 ">
+          <a href="#contact">
+          <button className="cursor-pointer text-white font-bold relative text-[17px] mr-8 px-6 py-2 text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px] hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-violet-700 focus:ring-violet-700 z-10 " >
               Hire Me 
             </button>
-            <button className="cursor-pointer text-white font-bold relative text-[17px] px-6 py-2 text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px]  hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-violet-700 focus:ring-violet-700 z-10">
+          </a>
+           <a href="https://docs.google.com/document/d/1IJZiH2N6wHmSg_wIhf3vmJAKCbuzPSDf--6ENJdTiLg/edit" target="_blank"> <button className="cursor-pointer text-white font-bold relative text-[17px] px-6 py-2 text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px]  hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-violet-700 focus:ring-violet-700 z-10">
             Online CV
-            </button>
+            </button></a>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
